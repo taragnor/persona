c01dee5ff598dfcd3e0dcb9e173691c2359e0d90
Joe Martino <taragnor@yahoo.com>
2023-12-21 11:36:09 -0500

    cleared test pad all esentials moved

--- src/module/datamodel/actor-types.ts
+++ src/module/datamodel/actor-types.ts
@@ -37,3 +37,18 @@ export class NPCSchema extends foundry.abstract.DataModel {
 
 export const ACTORMODELS = {pc: pcSchema, shadow: ShadowSchema, npc: NPCSchema} as const;
 
+	export class PersonaActor extends Actor<typeof ACTORMODELS> {
+
+		test() {
+			if (this.system.type == "shadow") {
+				this.system.shadowattack
+			}
+			if (this.system.type == "npc") {
+				this.system.schemaTest.num
+			}
+
+
+		}
+
+	}
+
