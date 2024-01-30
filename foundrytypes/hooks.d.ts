51891f3e65986835746083eb8c7e7aaea57d6b7e
Joe Martino <taragnor@yahoo.com>
2024-01-30 17:40:34 -0500

    Combat turn and round hooks added to Ftypes

--- foundrytypes/hooks.d.ts
+++ foundrytypes/hooks.d.ts
@@ -8,9 +8,25 @@ declare interface HOOKS {
 	"init": () => Promise<void>;
 	"ready": () => Promise<void>;
 	"updateCompendium": () => Promise<void>;
-	"applyActiveEffect": ApplyAEHookFn
-
+	"applyActiveEffect": ApplyAEHookFn;
+	"combatStart": (combat: Combat, updateData: CombatUpdateData) => Promise<void>;
+	"combatTurn": (combat: Combat, updateData: CombatUpdateData, updateOptions: CombatUpdateOptions) => Promise<void>;
+	"combatRound": (combat: Combat, updateData: CombatUpdateData, updateOptions: CombatUpdateOptions) => Promise<void>;
 };
 
 type ApplyAEHookFn = (actor: Actor<any,any>, change: AEChange , current: any , delta: any, changes: Record<string, any>) => Promise<void>;
 
+
+type CombatUpdateData = {
+	/** The current round of combat */
+	round:number,
+	/** The new turn number*/
+	turn: number
+}
+
+type CombatUpdateOptions = {
+	/**The amount of time in seconds that time is being advanced*/
+	advanceTime: number,
+	/** A signed integer for whether the turn order is advancing or rewinding */
+	direction: number
+}
