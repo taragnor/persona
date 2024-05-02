22d405e47249ca309fb379d2b89d2bae5621a995
Joe Martino <taragnor@yahoo.com>
2024-05-02 07:01:39 -0400

    global porgress clock integration

--- src/module/exploration/searchMenu.ts
+++ src/module/exploration/searchMenu.ts
@@ -7,13 +7,16 @@ import { PC } from "../actor/persona-actor.js";
 import { PersonaSockets } from "../persona.js";
 import { PersonaActor } from "../actor/persona-actor.js";
 import { UniversalActorAccessor } from "../utility/db-accessor.js";
+import { TensionPool } from "./tension-pool.js";
+
+
 
 export class SearchMenu {
 	private static dialog: Dialog | undefined = undefined;
 	static progress = {
 		treasuresFound: 0,
-			hazardFound: false,
-			secretFound: false
+		hazardFound: false,
+		secretFound: false
 	} ;
 	static promises : undefined | {
 		resolve: (data: SearchResult[] | PromiseLike<SearchResult[]>) => void;
@@ -38,263 +41,351 @@ export class SearchMenu {
 				this.leaveRoom(searchOptions);
 				break;
 			}
-			this.execSearch(results, searchOptions);
+			await this.execSearch(results, searchOptions);
 		}
 
 	}
 
-	static leaveRoom(options: SearchOptions) {
-		const progress = this.progress;
+	static leaveRoom(_options: SearchOptions) {
+		const _progress = this.progress;
 		ui.notifications.notify("left the room");
 	}
 
-	static execSearch(results : SearchResult[], options: SearchOptions) {
+	static async execSearch(results : SearchResult[], options: SearchOptions) {
+		let rolls : Roll[] = [];
 		ui.notifications.notify("executing search");
+		const guards = results.filter( x=> x.declaration == "guard").length;
+		for (const searcher of results) {
+			switch (searcher.declaration) {
+				case "search": {
+					const roll = new Roll("1d6");
+					searcher.result = await this.processSearchRoll([roll], options);
+					rolls.push(roll);
+					break;
+				}
+				case "careful-search": {
+					const roll = new Roll("1d6");
+					const roll2 = new Roll("1d10");
+					searcher.result = await this.processSearchRoll([roll, roll2], options);
+					rolls.push(roll, roll2);
+					break;
+				}
+				case "guard":
+					searcher.result = "nothing";
+					break;
+			}
+			switch (searcher.result) {
+				case "hazard":
+					this.progress.hazardFound = true;
+					if (options.stopOnHazard)
+						return;
+					break;
+				case "secret":
+					this.progress.secretFound = true;
+					break;
+				case "treasure":
+					options.treasureRemaining-= 1;
+					this.progress.treasuresFound +=1 ;
+					break;
+
+			}
+		}
+		const [roll, result] = await this.tensionPool(guards);
+		rolls.push(roll);
+		const html  =await renderTemplate(`${HBS_TEMPLATES_DIR}/search-result.hbs`, {tensionRoll : roll, results, options, tensionResult: result} );
+		const msg = ChatMessage.create({
+			speaker: {
+				scene: undefined,
+				actor: undefined,
+				token: undefined,
+				alias: "Search Results"
+			},
+			content: html,
+			type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+			rolls,
+		})
+		return msg;
 	}
 
-	static async searchOptionsDialog() : Promise<SearchOptions> {
-		let options = {
-			treasureRemaining: 3,
-			stopOnHazard: false,
-			isHazard: false,
-			isSecret: false
-		};
-		const html = await renderTemplate(`${HBS_TEMPLATES_DIR}/dialogs/searchOptions.hbs`, {options});
-		return await new Promise( (res , rej) => {
-			const dialog = new Dialog( {
-				title: "Search Options",
-				content: html,
-				close: () => rej ("Closed"),
-				buttons: {
-					submit: {
-						label: "Submit",
-						callback: (html: string) => {
-							console.log(html);
-							options.isSecret = $(html).find("#isSecret").is(":checked");
-							options.isHazard = $(html).find("#isHazard").is(":checked");
-							options.stopOnHazard = $(html).find("#stopOnHazard").is(":checked");
-							options.treasureRemaining = Number($(html).find(".treasureRemaining").val());
-							res(options);
+		static async processSearchRoll(rolls: Roll[], options: SearchOptions) : Promise<NonNullable<SearchResult["result"]>> {
+			for (const roll of rolls) {
+				await roll.roll();
+			}
+			const val = Math.max( ...rolls.map (x=> x.total))
+			switch (val) {
+				case 1:return options.isHazard ? "hazard" : "nothing";
+				case 2: return "nothing";
+				case 3: return options.isSecret ? "secret" : "nothing";
+				case 4:
+					return options.treasureRemaining >= 3 ? "treasure" : "nothing";
+				case 5:
+					return options.treasureRemaining >= 2 ? "treasure" : "nothing";
+				case 6:
+					return options.treasureRemaining >= 1 ? "treasure" : "nothing";
+				default: return "nothing";
+			}
+
+		}
+
+		static async tensionPool(guards: number) : Promise<[Roll, "ambush" | "battle" |"reaper" | "none"]> {
+			const tensionPool= TensionPool.inc();
+
+			const roll = new Roll(`${tensionPool}d6`);
+			roll.roll();
+			if (!roll.dice.some(dice => dice.values.some(v => v == 1)))
+			return [roll, "none"];
+			if (TensionPool.isMaxed() && roll.dice[0].values.filter( v=> v == 1 || v== 2))  {
+				return [roll, "reaper"];
+			}
+			if (roll.dice[0].values.filter( x=> x == 1 || x== 2).length > guards)
+			return [roll, "ambush"];
+			return [roll, "battle"];
+		}
+
+		static async searchOptionsDialog() : Promise<SearchOptions> {
+			let options = {
+				treasureRemaining: 3,
+				stopOnHazard: false,
+				isHazard: false,
+				isSecret: false
+			};
+			const html = await renderTemplate(`${HBS_TEMPLATES_DIR}/dialogs/searchOptions.hbs`, {options});
+			return await new Promise( (res , rej) => {
+				const dialog = new Dialog( {
+					title: "Search Options",
+					content: html,
+					close: () => rej ("Closed"),
+					buttons: {
+						submit: {
+							label: "Submit",
+							callback: (html: string) => {
+								console.log(html);
+								options.isSecret = $(html).find("#isSecret").is(":checked");
+								options.isHazard = $(html).find("#isHazard").is(":checked");
+								options.stopOnHazard = $(html).find("#stopOnHazard").is(":checked");
+								options.treasureRemaining = Number($(html).find(".treasureRemaining").val());
+								res(options);
+							}
 						}
 					}
-				}
-			}, {});
-			dialog.render(true);
-		});
-	}
-
-	static async openSearchWindow(options: SearchOptions): Promise<SearchResult[]> {
-		if (!game.user.isGM){
-			ui.notifications.error("Only the GM can call this function");
-			return [];
-		}
-		let searchUpdate: SearchUpdate ={
-			results: this.generateOriginalSearchResults(),
-			options
-		};
-		this.onSearchUpdate(searchUpdate);
-		return new Promise( (res, rej) => {
-			this.promises = {
-				resolve: res,
-				reject: rej,
-			};
-		});
-	}
-
-	private static generateOriginalSearchResults() : SearchResult[] {
-		return (game.scenes.active.tokens.contents as TokenDocument<PersonaActor>[])
-			.filter( x=> x.actor
-				&& x.actor.system.type == "pc"
-				&& x.actor.focii.length > 0 //check to eliminate crunched party token
-			)
-			.flatMap( tok=> {
-				const actor = tok.actor! as PC;
-				const activePlayers = game.users.contents
-					.filter( user => user.active && !user.isGM);
-				const owner = activePlayers
-					.find( user => user.character == actor)
-					?? activePlayers.find( user=> actor.testUserPermission(user, "OWNER")) 
-					?? game.users.find(x=> x.isGM && x.active);
-				if (!owner) return [];
-				const ret : SearchResult = {
-					searcher: {
-						actor: {
-							token: undefined,
-							actorId: actor.id,
-						},
-						name: actor.displayedName,
-						ownerId: owner.id,
-						ownerName: owner.name,
-					},
-					declaration: "undecided"
-				}
-				return [ret];
+				}, {});
+				dialog.render(true);
 			});
-	}
-
-	private static sendUpdate() {
-		if (this.data)
-			PersonaSockets.simpleSend("SEARCH_UPDATE", this.data, game.users.contents.filter(x=> x.active).map( x=> x.id));
-	}
-
-	static async onSearchUpdate(updateData  : SearchUpdate, send: boolean  = true) {
-		this.data = updateData;
-		if (!this.isOpen()) {
-			this.openDialog(updateData);
-		} else {
-			this.updateDialog();
 		}
-		if (send)
-			this.sendUpdate();
-	}
 
-	private static async openDialog( updateData: SearchUpdate) : Promise<Dialog> {
-		const html = await this.renderTemplate();
-		this.dialog = new Dialog( {
-			title: "Search Actions",
-			content: html,
-			render: (html) => this.setListeners(html),
-			buttons: {},
-			close: () => this.isOpen() ? this.openDialog(this.data!) : this.closeDialog(),
-		}, {});
-		this.dialog.render(true);
-		this.data = updateData;
-		return this.dialog;
-	}
-
-	static closeDialog() {
-		this.data = null;
-		this.dialog = undefined;
-	}
-
-	private static async updateDialog() {
-		if (!this.dialog) throw new PersonaError("Dialog isn't open!");
-		if (!this.data) throw new PersonaError("Can't find Data");
-		ui.notifications.notify("Updating Dialog");
-		const html = await this.renderTemplate();
-		this.dialog.element.find(".dialog-content").children().replaceWith(html);
-		this.setListeners(this.dialog.element);
-		if ( this.checkResolution())
-			this.close();
-	}
-
-	private static checkResolution() : boolean{
-		if (!this.data) return false;
-		const results = this.data.results;
-		if (results.some (res => res.declaration == "undecided" || res.declaration == "disconnected")){ 
-			return false;
-		}
-		const leavers = results.filter(res => res.declaration == "leave");
-		if (leavers.length > 0) {
-			return leavers.length >= results.length / 2;
-		}
-		return true;
-	}
-
-	static close() {
-		if (!this.promises && game.user.isGM)
-			throw new PersonaError("No promise data found!")
-		if (!this.data)
-			throw new PersonaError("No data found!")
-		if (this.promises) {
-			this.promises.resolve(this.data.results);
-			this.promises = undefined;
-		}
-		const dialog = this.dialog;
-		this.dialog = undefined;
-		this.data = null;
-		if (dialog)
-			dialog.close();
-	}
-
-	private static async renderTemplate() : Promise<string> {
-		const updateData = this.data!;
-		const html = await renderTemplate( `${HBS_TEMPLATES_DIR}/dialogs/search-dialog.hbs`, {data: updateData, SEARCH_ACTIONS});
-		return html;
-	}
-
-	private static setListeners(html: string | JQuery<HTMLElement>) {
-		if (typeof html == "string")
-			html = $(html);
-		html.find(".action-choice").on("change", ev=> {
-			ev.preventDefault();
-			ev.stopPropagation();
-			const target = $(ev.currentTarget);
-			const actorId = HTMLTools.getClosestData(ev, "actorId");
-			const newAction = String(target.val());
-			if (!this.data) {
-				throw new PersonaError(`No Data Found`);
-				}
-			const sdata = this.data.results.find( res => res.searcher.actor.actorId == actorId);
-			if (!sdata) {
-				throw new PersonaError(`Can't find actor ${actorId}`);
+		static async openSearchWindow(options: SearchOptions): Promise<SearchResult[]> {
+			if (!game.user.isGM){
+				ui.notifications.error("Only the GM can call this function");
+				return [];
 			}
-			sdata.declaration = newAction as SearchAction;
-			this.onSearchUpdate(this.data);
-		});
-	}
-
-
-	static onUserConnect(_user: FoundryUser) {
-		if (this.isOpen() && this.data) {
-			this.onSearchUpdate(this.data);
+			let searchUpdate: SearchUpdate ={
+				results: this.generateOriginalSearchResults(),
+				options
+			};
+			this.onSearchUpdate(searchUpdate);
+			return new Promise( (res, rej) => {
+				this.promises = {
+					resolve: res,
+					reject: rej,
+				};
+			});
 		}
-	}
 
-	 static onUserDisconnect(user: FoundryUser) {
-		if (this.isOpen() && this.data) {
+		private static generateOriginalSearchResults() : SearchResult[] {
+			return (game.scenes.active.tokens.contents as TokenDocument<PersonaActor>[])
+				.filter( x=> x.actor
+					&& x.actor.system.type == "pc"
+					&& x.actor.focii.length > 0 //check to eliminate crunched party token
+				)
+				.flatMap( tok=> {
+					const actor = tok.actor! as PC;
+					const activePlayers = game.users.contents
+						.filter( user => user.active && !user.isGM);
+					const owner = activePlayers
+						.find( user => user.character == actor)
+						?? activePlayers.find( user=> actor.testUserPermission(user, "OWNER")) 
+						?? game.users.find(x=> x.isGM && x.active);
+					if (!owner) return [];
+					const ret : SearchResult = {
+						searcher: {
+							actor: {
+								token: undefined,
+								actorId: actor.id,
+							},
+							name: actor.displayedName,
+							ownerId: owner.id,
+							ownerName: owner.name,
+						},
+						declaration: "undecided"
+					}
+					return [ret];
+				});
+		}
+
+		private static sendUpdate() {
+			if (this.data)
+				PersonaSockets.simpleSend("SEARCH_UPDATE", this.data, game.users.contents.filter(x=> x.active).map( x=> x.id));
+		}
+
+		static async onSearchUpdate(updateData  : SearchUpdate, send: boolean  = true) {
+			this.data = updateData;
+			if (!this.isOpen()) {
+				this.openDialog(updateData);
+			} else {
+				this.updateDialog();
+			}
+			if (send)
+				this.sendUpdate();
+		}
+
+		private static async openDialog( updateData: SearchUpdate) : Promise<Dialog> {
+			const html = await this.renderTemplate();
+			this.dialog = new Dialog( {
+				title: "Search Actions",
+				content: html,
+				render: (html) => this.setListeners(html),
+				buttons: {},
+				close: () => this.isOpen() ? this.openDialog(this.data!) : this.closeDialog(),
+			}, {});
+			this.dialog.render(true);
+			this.data = updateData;
+			return this.dialog;
+		}
+
+		static closeDialog() {
+			this.data = null;
+			this.dialog = undefined;
+		}
+
+		private static async updateDialog() {
+			if (!this.dialog) throw new PersonaError("Dialog isn't open!");
+			if (!this.data) throw new PersonaError("Can't find Data");
+			ui.notifications.notify("Updating Dialog");
+			const html = await this.renderTemplate();
+			this.dialog.element.find(".dialog-content").children().replaceWith(html);
+			this.setListeners(this.dialog.element);
+			if ( this.checkResolution())
+				this.close();
+		}
+
+		private static checkResolution() : boolean{
+			if (!this.data) return false;
 			const results = this.data.results;
-			results.filter( item => item.searcher.ownerId == user.id)
-			.forEach( item => item.declaration = "disconnected");
-			this.onSearchUpdate(this.data);
+			if (results.some (res => res.declaration == "undecided" || res.declaration == "disconnected")){ 
+				return false;
+			}
+			const leavers = results.filter(res => res.declaration == "leave");
+			if (leavers.length > 0) {
+				return leavers.length >= results.length / 2;
+			}
+			return true;
+		}
+
+		static close() {
+			if (!this.promises && game.user.isGM)
+				throw new PersonaError("No promise data found!")
+			if (!this.data)
+				throw new PersonaError("No data found!")
+			if (this.promises) {
+				this.promises.resolve(this.data.results);
+				this.promises = undefined;
+			}
+			const dialog = this.dialog;
+			this.dialog = undefined;
+			this.data = null;
+			if (dialog)
+				dialog.close();
+		}
+
+		private static async renderTemplate() : Promise<string> {
+			const updateData = this.data!;
+			const html = await renderTemplate( `${HBS_TEMPLATES_DIR}/dialogs/search-dialog.hbs`, {data: updateData, SEARCH_ACTIONS});
+			return html;
+		}
+
+		private static setListeners(html: string | JQuery<HTMLElement>) {
+			if (typeof html == "string")
+				html = $(html);
+			html.find(".action-choice").on("change", ev=> {
+				ev.preventDefault();
+				ev.stopPropagation();
+				const target = $(ev.currentTarget);
+				const actorId = HTMLTools.getClosestData(ev, "actorId");
+				const newAction = String(target.val());
+				if (!this.data) {
+					throw new PersonaError(`No Data Found`);
+				}
+				const sdata = this.data.results.find( res => res.searcher.actor.actorId == actorId);
+				if (!sdata) {
+					throw new PersonaError(`Can't find actor ${actorId}`);
+				}
+				sdata.declaration = newAction as SearchAction;
+				this.onSearchUpdate(this.data);
+			});
+		}
+
+
+		static onUserConnect(_user: FoundryUser) {
+			if (this.isOpen() && this.data) {
+				this.onSearchUpdate(this.data);
+			}
+		}
+
+		static onUserDisconnect(user: FoundryUser) {
+			if (this.isOpen() && this.data) {
+				const results = this.data.results;
+				results.filter( item => item.searcher.ownerId == user.id)
+					.forEach( item => item.declaration = "disconnected");
+				this.onSearchUpdate(this.data);
+			}
+		}
+
+	} // end of class
+
+	Hooks.on("userConnected", (user: FoundryUser, isConnectionEvent: boolean) => {
+		if (game.user.isGM && SearchMenu.isOpen()) {
+			if (isConnectionEvent) {
+				SearchMenu.onUserConnect(user);
+			} else {
+				SearchMenu.onUserDisconnect(user);
+			}
+		}
+	});
+
+
+	type SearchOptions = {
+		treasureRemaining: number;
+		stopOnHazard?: boolean;
+		isHazard: boolean;
+		isSecret: boolean;
+	};
+
+	type SearchResult = {
+		searcher : SearcherData;
+		result?: "nothing" | "treasure" | "hazard" | "secret";
+		roll?: number;
+		declaration: SearchAction ;
+	};
+
+	type SearcherData = {
+		actor: UniversalActorAccessor<PersonaActor>;
+		name: string;
+		ownerId: string;
+		ownerName: string;
+	}
+
+	type SearchUpdate = {
+		results: SearchResult[];
+		options: SearchOptions;
+	};
+
+	Hooks.on("socketsReady", () => PersonaSockets.setHandler("SEARCH_UPDATE", (x: SearchUpdate) => SearchMenu.onSearchUpdate( x, false))
+	);
+
+
+	declare global {
+		export interface SocketMessage {
+			"SEARCH_UPDATE": SearchUpdate;
 		}
 	}
-
-} // end of class
-
-Hooks.on("userConnected", (user: FoundryUser, isConnectionEvent: boolean) => {
-	if (game.user.isGM && SearchMenu.isOpen()) {
-		if (isConnectionEvent) {
-			SearchMenu.onUserConnect(user);
-		} else {
-			SearchMenu.onUserDisconnect(user);
-		}
-	}
-});
-
-
-type SearchOptions = {
-	treasureRemaining: number;
-	stopOnHazard?: boolean;
-	isHazard: boolean;
-	isSecret: boolean;
-};
-
-type SearchResult = {
-	searcher : SearcherData;
-	result?: "nothing" | "treasure" | "hazard";
-	declaration: SearchAction ;
-};
-
-type SearcherData = {
-	actor: UniversalActorAccessor<PersonaActor>;
-	name: string;
-	ownerId: string;
-	ownerName: string;
-}
-
-type SearchUpdate = {
-	results: SearchResult[];
-	options: SearchOptions;
-};
-
-Hooks.on("socketsReady", () => PersonaSockets.setHandler("SEARCH_UPDATE", (x: SearchUpdate) => SearchMenu.onSearchUpdate( x, false))
-);
-
-
-declare global {
-	export interface SocketMessage {
-		"SEARCH_UPDATE": SearchUpdate;
-	}
-}
 
---
+++ src/module/exploration/tension-pool.ts
@@ -0,0 +1,75 @@
+declare global {
+	interface Window {
+		TensionPool: typeof TensionPool;
+		clockDatabase: undefined | {
+			getName(name: string) : Clock | undefined;
+			update(newData : {id: string, value: number}): Promise<void>;
+		}
+	}
+}
+
+type Clock = {
+	name: string,
+	id: string,
+	value: number,
+	max: number,
+	private: boolean;
+
+}
+
+
+export class TensionPool {
+	static _amt: number;
+	static DEFAULT_MAX= 6;
+
+	static get amt() : number {
+		if (window.clockDatabase) {
+			const clock = window.clockDatabase.getName("Tension Pool");
+			if (clock) {
+				this._amt = clock.value;
+				return clock.value;
+			}
+		}
+		return this._amt;
+
+	}
+
+	static async inc(): Promise<number> {
+		if (!this.isMaxed())
+			await this.modifyTensionPool(1);
+		return this.amt;
+	}
+
+	private static async refreshValue(amt: number) {
+		if (window.clockDatabase) {
+			const clock = window.clockDatabase.getName("Tension Pool");
+			if (!clock) return;
+			await window.clockDatabase.update( {id: clock.id, value: amt})
+		}
+
+	}
+
+	static isMaxed() : boolean {
+		return this.amt >= this.max();
+	}
+
+	static max(): number {
+		if (window.clockDatabase) {
+			const clock = window.clockDatabase.getName("Tension Pool");
+			if (clock)  {
+				return clock.max;
+			}
+		}
+		return this.DEFAULT_MAX;
+	}
+
+	static async modifyTensionPool(mod : number): Promise<number> {
+		this._amt = Math.min(this.max(), Math.max(0, this.amt + mod));
+		await this.refreshValue(this._amt);
+		return this.amt;
+	}
+
+}
+
+
+window.TensionPool = TensionPool;
