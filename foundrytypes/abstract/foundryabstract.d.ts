6e6026348c7396f20f3f1cb60021d939f7d39aeb
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2024-12-17 23:41:22 -0500

    Add DatabaseCreateOperation interface

--- foundrytypes/abstract/foundryabstract.d.ts
+++ foundrytypes/abstract/foundryabstract.d.ts
@@ -3,4 +3,23 @@ interface FoundryAbstract {
 	TypeDataModel : typeof TypeDataModelClass;
 }
 
-
+interface DatabaseCreateOperation {
+	broadcast?: boolean;
+	data?: object[];
+	// Default: false
+	keepId?: boolean;
+	// Default: true
+	keepEmbeddedIds?: boolean;
+	modifiedTime?: number;
+	// Default: false
+	noHook?: boolean;
+	// Default: true
+	render?: boolean;
+	// Default: false
+	renderSheet?: boolean;
+	// Default: null
+	parent?: any;
+	pack?: string | null;
+	parentUuid?: string | null;
+	_result?: (string | object)[];
+}
