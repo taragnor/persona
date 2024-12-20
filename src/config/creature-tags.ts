6e4a0c0fc32586d11453799ab9552d2c4e7d8377
Joe Martino <taragnor@yahoo.com>
2024-12-20 17:27:51 -0500

    add: more creature tags

--- src/config/creature-tags.ts
+++ src/config/creature-tags.ts
@@ -2,6 +2,16 @@ export const CREATURE_TAG_LIST = [
 	"neko",
 	"cu",
 	"fairy",
+	"foul",
+	"human",
+	"elemental",
+	"beast",
+	"machine",
+	"god",
+	"avian",
+	"dragon",
+	"evil-spirit",
+
 ] as const;
 
 export type CreatureTag = typeof CREATURE_TAG_LIST[number];
