a1bfc30e8aa1d4ba55595f348a7d25f7783f062f
Joe Martino <taragnor@yahoo.com>
2025-07-06 18:25:26 -0400

    consequence V2

---
+++ src/config/consequence-type-v2.ts
@@ -0,0 +1,90 @@
+type ConsStruct =  ConsequenceNode
+
+type MenuItem = {
+	name: string,
+	consStruct: ConsStruct,
+}
+
+type ConsStructToCons<T extends ConsStruct> = 
+	T extends ConsequenceNode<any> ? ConsNodeToCons<T> :
+	unknown;
+
+
+
+type ConsequenceNode<T extends FieldDescriptor[] = FieldDescriptor[]> = {
+	type: "cons",
+	data: Readonly<T>,
+}
+
+type ConsNodeToCons<T extends ConsequenceNode> = FieldDescriptorToObject<T["data"]>;
+
+
+type DataTypeMap = {
+	string: string;
+	number: number;
+	menu: MenuItem;
+};
+type FieldDescriptor =
+	GenericFieldDescriptor | MenuFieldDescriptor;
+
+type GenericFieldDescriptor<T extends keyof DataTypeMap = keyof DataTypeMap> = {
+	name: string;
+	dataType: T;
+	choices ?: DataTypeMap[Exclude<T,"menu">][];
+};
+
+type MenuFieldDescriptor =
+{
+	name: string;
+	dataType: "menu";
+	choices: MenuItem[];
+};
+
+
+type ParseMenuItems<T extends MenuItem[], MenuFieldName extends string> =
+	{[K in T[number] as K["name"]]: ObjectProp<MenuFieldName, K["name"]>
+		&  ConsStructToCons<K["consStruct"]>
+	};
+
+	type FieldDescriptorToObject<T extends readonly FieldDescriptor[]> =
+	GenericFieldDescriptorObject<T> &
+	MenuFieldUnion<T>
+	// ObjToValueUnion<
+	// {
+	// 	[K in T[number] as K["name"]]:
+	// 	K extends MenuFieldDescriptor
+	// 	? ParseMenuItems<K["choices"], K["name"]>
+	// 	: {}
+	// }>;
+
+	type GenericFieldDescriptorObject <T extends readonly FieldDescriptor[]> =
+	{
+		[K in T[number] as (K extends MenuFieldDescriptor ? never : K["name"] )]:
+		K extends MenuFieldDescriptor
+		? never
+		: K["choices"] extends Array<any>
+		? K["choices"][number]
+		: DataTypeMap[K["dataType"]]
+	};
+
+type MenuFieldUnion<T extends readonly FieldDescriptor[]> =
+	ObjToValueUnion< {
+		[K in T[number] as K["dataType"] extends "menu" ? K["name"] : never]:
+		K extends MenuFieldDescriptor
+		? ObjToValueUnion<ParseMenuItems<K["choices"], K["name"]>>
+		: {}
+	}>;
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type ObjectProp<S extends string, X extends any> = {
+[V in S]: X
+}
+
+type ObjToValueUnion<T> = T[keyof T] extends never ? {} : T[keyof T];
+
+
+
+type xxx = ObjToValueUnion<{}>
--- src/testpad.ts
+++ src/testpad.ts
@@ -1,15 +1,117 @@
-import { ValidAttackers } from "./module/combat/persona-combat.js";
 
 
-function f( actor: ValidAttackers) {
-	const situation : Situation = {
-		user : actor.accessor,
-		naturalRoll: 12,
-		rollTags: [],
-		rollTotal : 12,
+const simpleField =
+	{name: "simple", dataType: "string"} as const satisfies FieldDescriptor;
+
+const simplestruct = {
+	type: "cons",
+	data: [{name: "id", dataType: "string"}],
+}  as const satisfies ConsStruct;
+
+const Menu2 = {
+	name: "targetType",
+	dataType: "menu",
+	choices: [{
+		name: "user",
+		consStruct: simplestruct,
+	},{
+		name: "other",
+		consStruct: simplestruct
+	},
+	],
+} as const satisfies MenuFieldDescriptor;
+
+
+const X =[ {
+	name: "x",
+	dataType: "string",
+	choices: ["A", "B", "C", "D"],
+}, {
+	name: "el",
+	dataType: "number",
+}, Menu2
+] as const satisfies FieldDescriptor[] ;
+
+const Y = [ {
+	name: "y",
+	dataType: "string",
+	choices: ["E", "F"],
+
+},
+] as const satisfies FieldDescriptor[] ;
+
+const XConstruct = {
+	type: "cons",
+	data: X,
+} as const satisfies ConsequenceNode;
+
+const YConstruct = {
+	type: "cons",
+	data: Y,
+} as const satisfies ConsequenceNode;
+
+type test = FieldDescriptorToObject<typeof X>;
+
+type test2 = ConsStructToCons<typeof XConstruct>;
+
+const Menu = {
+	name: "m1",
+	dataType: "menu",
+	choices: [{
+		name: "X-Option",
+		consStruct: XConstruct,
+	},{
+		name: "Y-Option",
+		consStruct:YConstruct,
+	},
+	],
+} as const satisfies MenuFieldDescriptor;
+
+
+
+const MenuStruct = {
+	type: "cons",
+	data: [Menu, simpleField],
+} as const satisfies ConsequenceNode;
+
+type test3 = ConsStructToCons<typeof MenuStruct>;
+
+type test4 = Prettify<ParseMenuItems<typeof Menu["choices"], "m1">>;
+
+type test5 = MenuFieldUnion<typeof Menu[]>;
+type test6 = MenuFieldUnion<typeof simpleField[]>;
+type test7 = GenericFieldDescriptorObject<typeof simpleField[]>;
+type test8 = GenericFieldDescriptorObject<typeof Menu[]>;
+
+type targetTest = Prettify<ConsStructToCons<typeof XConstruct>>;
+	type targetTest1 = Prettify<GenericFieldDescriptorObject<typeof X>>;
+	type targetTest2 = Prettify<MenuFieldUnion<typeof XConstruct.data>>;
+
+
+type Increment<N extends number, T extends any[] = []> = 
+  [...T, any]['length'] extends N
+    ? [...T, any, any]['length']
+    : Increment<N, [...T, any]>;
+
+//@ts-ignore
+let tt : TargetTest = something;
+
+
+//@ts-ignore
+let x : test3 = something;
+//@ts-ignore
+let y : test4 = something;
+
+//@ts-ignore
+let z : test5 = something;
+
+y["X-Option"].targetType
+
+if (x.m1 == "X-Option") {
+	if (x.targetType == "user") {
+		x.id
 	}
+
 }
 
 
-
-ui.notifications.error
