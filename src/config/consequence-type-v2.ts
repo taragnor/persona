142dc4527d31821ec245e9335cbd28454f1f2932
Joe Martino <taragnor@yahoo.com>
2025-07-07 17:23:47 -0400

    result function (still need detection on final only)

--- src/config/consequence-type-v2.ts
+++ src/config/consequence-type-v2.ts
@@ -1,80 +1,87 @@
-type ConsStruct =  ConsequenceNode
+import { CombatResult } from "../module/combat/combat-result.js";
 
-type MenuItem = {
-	name: string,
-	consStruct: ConsStruct,
-}
+declare global {
+	type ConsStruct =  ConsequenceNode
 
-type ConsStructToCons<T extends ConsStruct> = 
-	Prettify< T extends ConsequenceNode<any> ? ConsNodeToCons<T> :
-	unknown>;
+	type MenuItem = {
+		name: string,
+		consStruct: ConsStruct,
+	}
 
+	type ConsStructToCons<T extends ConsStruct> = 
+		Prettify< T extends ConsequenceNode<any> ? ConsNodeToCons<T> :
+		unknown>;
 
-
-type ConsequenceNode<T extends FieldDescriptor[] = FieldDescriptor[]> = {
-	type: "cons",
-	data: Readonly<T>,
-}
-
-type ConsNodeToCons<T extends ConsequenceNode> = FieldDescriptorToObject<T["data"]>;
+	type ToResultFn = (node: ConsNodeToCons<ConsequenceNode>,sit: Situation) => CombatResult;
 
 
-type DataTypeMap = {
-	string: string;
-	number: number;
-	menu: MenuItem;
-};
-type FieldDescriptor =
-	GenericFieldDescriptor | MenuFieldDescriptor;
+	type ConsequenceNode<T extends FieldDescriptor[] = FieldDescriptor[]> = {
+		type: "cons",
+		data: Readonly<T>,
+		toResult ?: ToResultFn
+	}
 
-type GenericFieldDescriptor<T extends keyof DataTypeMap = keyof DataTypeMap> = {
-	name: string;
-	dataType: T;
-	choices ?: DataTypeMap[Exclude<T,"menu">][];
-};
-
-type MenuFieldDescriptor =
-{
-	name: string;
-	dataType: "menu";
-	choices: MenuItem[];
-};
+	type ConsNodeToCons<T extends ConsequenceNode> = FieldDescriptorToObject<T["data"]>;
 
 
-type ParseMenuItems<T extends MenuItem[], MenuFieldName extends string> =
-	{[K in T[number] as K["name"]]: ObjectProp<MenuFieldName, K["name"]>
-		&  ConsStructToCons<K["consStruct"]>
+	type DataTypeMap = {
+		string: string;
+		number: number;
+		menu: MenuItem;
 	};
+	type FieldDescriptor =
+		GenericFieldDescriptor | MenuFieldDescriptor;
+
+	type GenericFieldDescriptor<T extends keyof DataTypeMap = keyof DataTypeMap> = {
+		name: string;
+		dataType: T;
+		choices ?: DataTypeMap[Exclude<T,"menu">][];
+		default ?:  DataTypeMap[Exclude<T,"menu">];
+	};
+
+	type MenuFieldDescriptor =
+		{
+			name: string;
+			dataType: "menu";
+			choices: MenuItem[];
+		};
+
+
+	type ParseMenuItems<T extends MenuItem[], MenuFieldName extends string> =
+		{[K in T[number] as K["name"]]: ObjectProp<MenuFieldName, K["name"]>
+			&  ConsStructToCons<K["consStruct"]>
+		};
 
 	type FieldDescriptorToObject<T extends readonly FieldDescriptor[]> =
-	GenericFieldDescriptorObject<T> &
-	MenuFieldUnion<T>
+		GenericFieldDescriptorObject<T> &
+		MenuFieldUnion<T>
 
-	type GenericFieldDescriptorObject <T extends readonly FieldDescriptor[]> =
-	{
-		[K in T[number] as (K extends MenuFieldDescriptor ? never : K["name"] )]:
-		K extends MenuFieldDescriptor
-		? never
-		: K["choices"] extends Array<any>
-		? K["choices"][number]
-		: DataTypeMap[K["dataType"]]
-	};
+		type GenericFieldDescriptorObject <T extends readonly FieldDescriptor[]> =
+		{
+			[K in T[number] as (K extends MenuFieldDescriptor ? never : K["name"] )]:
+			K extends MenuFieldDescriptor
+			? never
+			: K["choices"] extends Array<any>
+			? K["choices"][number]
+			: DataTypeMap[K["dataType"]]
+		};
 
-type MenuFieldUnion<T extends readonly FieldDescriptor[]> =
-	ObjToValueUnion< {
-		[K in T[number] as K["dataType"] extends "menu" ? K["name"] : never]:
-		K extends MenuFieldDescriptor
-		? ObjToValueUnion<ParseMenuItems<K["choices"], K["name"]>>
-		: {}
-	}>;
+	type MenuFieldUnion<T extends readonly FieldDescriptor[]> =
+		ObjToValueUnion< {
+			[K in T[number] as K["dataType"] extends "menu" ? K["name"] : never]:
+			K extends MenuFieldDescriptor
+			? ObjToValueUnion<ParseMenuItems<K["choices"], K["name"]>>
+			: {}
+		}>;
 
-type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
+	type Prettify<T> = {
+		[K in keyof T]: T[K];
+	} & {};
 
-type ObjectProp<S extends string, X extends any> = {
-[V in S]: X
+	type ObjectProp<S extends string, X extends any> = {
+		[V in S]: X
+	}
+
+	type ObjToValueUnion<T> = T[keyof T] extends never ? {} : T[keyof T];
+
 }
-
-type ObjToValueUnion<T> = T[keyof T] extends never ? {} : T[keyof T];
-
